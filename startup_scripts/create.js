a18d79e070a6b091ddeba0b742dde60877e120ce
Peixes <guilhermeoficial074@gmail.com>
2025-07-30 20:34:30 -0300

    Adição do item 'woodenmechanism'

--- startup_scripts/create.js
+++ startup_scripts/create.js
@@ -1,1 +1,7 @@
 //Mecanismos Tier 1
+StartupEvents.registry('item', event => {
+    //Mecanismos
+    event.create('woodenmechanism')
+    .displayName('Mecanismo de madeira')
+    .maxStackSize(16)
+})
